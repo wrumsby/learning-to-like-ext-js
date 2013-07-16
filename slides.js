3b758c707b0f81c224c81478aa9bc231392e7146
Walter Rumsby <walter@rumsby.net>
2013-07-17 08:28:03 +1200

    First commit of slides
    High-level outline

---
+++ slides.js
@@ -0,0 +1,81 @@
+var SLIDES = [
+	{
+		"cls": "first",
+		"title": "Learning to Like ExtJS"
+	},
+
+	{
+		"list": ["heavyweight DOM", "opinionated", "monolithic", "steep learning curve"],
+		"image": "http://www.flickr.com/photos/edmands/3684542795/"
+	},
+
+	{
+		"list": ["large set of controls", "capable custom event system", "extensive documentation", "builds on a set of core ideas"],
+		"image": "http://www.flickr.com/photos/tracilawson/1312541390/"
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/pagedooley/2133417156/",
+		"notes": "jQuery - DOM centric; Backbone - evented views; Ext - components (Flight, Marionette?)"
+	},
+
+	{
+		"notes": "quotes from jQuery folks on why jQuery isn't enough"
+	},
+
+	{
+		"notes": "Ext.define and Ext.create"
+	},
+
+	{
+		"notes": "config in define and create"
+	},
+
+	{
+		"notes": "components"
+	},
+
+	{
+		"notes": "containers"
+	},
+
+	{
+		"notes": "xtype syntax"
+	},
+
+	{
+		"notes": "listeners"
+	},
+
+	{
+		"notes": "xtemplates"
+	},
+
+	{
+		"notes": "stores"
+	},
+
+	{
+		"notes": "MVC - when to use"
+	},
+
+	{
+		"notes": "DOM - Rosetta; DOM Helper"
+	},
+
+	{
+		"notes": "Content"
+	},
+
+	{
+		"notes": "ExtJS documentation - API reference; kitchen sink"
+	},
+
+	{
+		"notes": "devdocs.io"
+	},
+
+	{
+		"notes": "JSHint - .jshintrc, WebEssentials, grunt"
+	}
+];
