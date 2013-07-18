c4de616278f4cabbfc8ad9b7528ca911ad2adf70
Walter Rumsby <walter@rumsby.net>
2013-07-18 22:14:41 +1200

    Update to slides

--- slides.js
+++ slides.js
@@ -10,7 +10,7 @@ var SLIDES = [
 	},
 
 	{
-		"list": ["large set of controls", "capable custom event system", "extensive documentation", "builds on a set of core ideas"],
+		"list": ["large set of controls", "capable custom event system", "extensive documentation", "builds on a set of core ideas", "ARIA roles", "layouts (maybe)", "lifecycle"],
 		"image": "http://www.flickr.com/photos/tracilawson/1312541390/"
 	},
 
@@ -20,62 +20,80 @@ var SLIDES = [
 	},
 
 	{
+		"quote": {
+			"text":	"The mantra of jQuery is \"get some elements, do something with them\" - perfectly terrible preparation for analyzing an application from a perspective other than the DOM.",
+			"from":	"Rebecca Murphey"
+		},
+		"image": "http://www.flickr.com/photos/37108241@N00/61056391/",
+		"opinion": true,
 		"notes": "quotes from jQuery folks on why jQuery isn't enough"
 	},
 
 	{
-		"notes": "Ext.define and Ext.create"
+		"gist": "https://gist.github.com/wrumsby/6027918",
+		"notes": "Ext.define - extend, renderTo, listeners; config defauls; namespace"
 	},
 
 	{
-		"notes": "config in define and create"
+		"gist": "https://gist.github.com/wrumsby/6027937",
+		"notes": "Ext.create - redefine renderTo; namespace"
 	},
 
 	{
-		"notes": "components"
+		"gist": "https://gist.github.com/wrumsby/6027943",
+		"notes": "Creating a component - renderTo"
 	},
 
 	{
-		"notes": "containers"
+		"gist": "https://gist.github.com/wrumsby/6027987",
+		"notes": "Containers are components that contain other components (composite pattern); items"
 	},
 
 	{
-		"notes": "xtype syntax"
+		"gist": "https://gist.github.com/wrumsby/6028034",
+		"notes": "declarative syntax"
 	},
 
 	{
-		"notes": "listeners"
+		"gist": "https://gist.github.com/wrumsby/6028064",
+		"notes": "declarative syntax; componet query; listeners"
 	},
 
 	{
-		"notes": "xtemplates"
+		"gist": "https://gist.github.com/wrumsby/6028092",
+		"notes": "xtemplates - not encoded, member functions"
 	},
 
 	{
-		"notes": "stores"
+		"gist": "https://gist.github.com/wrumsby/6028125",
+		"notes": "stores - proxies, readers"
 	},
 
 	{
+		"image": "http://www.flickr.com/photos/71753457@N00/189511858/in/photolist-hKigQ-hSu3g-nqmVF-Amso1-FzzRy-2hhWr2-2T4bqA-36c56S-37Ct3i-4W14KT-4W14TH-4W5iU1-4W5j2Q-4W5j4w-54xTq7-55c1KB-5jyog8-5jypMD-5piDPm-5CjkUg-6dq2bP-6dq2gK-6dq2nB-6dq2tn-6dua41-6duauY-6duaBb-6CNzes-794rfz-794rEv-798iN1-798jbQ-7sqdvA-7sqdNo-7sqeeE-7sqepo-7sqeR9-a5b44N-atBUcv-ah6Wr7-9BYnNU-9BVt1P-9BYrVJ-8NwNnX-8NwP4K-atCghe-95fd6H-byp86A-9LXVvg-9MALpj-buxneW",
 		"notes": "MVC - when to use"
 	},
 
 	{
-		"notes": "DOM - Rosetta; DOM Helper"
-	},
-
-	{
-		"notes": "Content"
-	},
-
-	{
+		"image": "http://www.flickr.com/photos/wrumsby/9311252763/",
 		"notes": "ExtJS documentation - API reference; kitchen sink"
 	},
 
 	{
+		"image": "http://www.flickr.com/photos/wrumsby/9314057358/",
 		"notes": "devdocs.io"
 	},
 
 	{
+		"gist": "https://gist.github.com/twalker/4302547",
 		"notes": "JSHint - .jshintrc, WebEssentials, grunt"
-	}
+	},
+
+	{
+		"image": "http://www.flickr.com/photos/29965372@N04/8314213248/in/photolist-dEGvWh-8xAUfM-9wCXZN-8NBt5P-8mkkKE-bz6rdn-bmrsGf-7PxCkm-8mnB37-8J1UNj-ciV5vu-e4cnNC-93TTGC-8VUuEa-9QTdmk-dpX9R1-7Wcnia-9Y11w9-ax21xS-bVqh1g-8vgQV4-9EoJYR-8uaaTi-fawHeR-8r4wFY-7AAjaf-f1ziUh-bGydtz-ax4dFt-7R5XW9-8d8PEK-86MR3X-8ik4vs-7QmWB5-8igLn6-aDcz8k-afKvp1-8jmPoP-9EeDgP-bddzZ2-8sNTqR-bqykj8-bjWm74-asF38k"
+	},
+
+	{
+		"credits": true
+	}	
 ];
